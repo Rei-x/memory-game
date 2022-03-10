b0a618083d916b37f30d6e6e411ebeea16e26d15
Rei-x <wszyzdgo@gmail.com>
2022-03-10 15:11:02 +0000

    Fix build

--- cli/createEnvFromJson.ts
+++ cli/createEnvFromJson.ts
@@ -1,6 +1,13 @@
-import configJson from '../firebaseConfig.json';
+import fs from 'fs';
 
-process.env[`FIREBASE_CONFIG`] = JSON.stringify(configJson);
+console.log(__dirname);
 
-console.log(`FIREBASE_CONFIG=${process.env[`FIREBASE_CONFIG`]}`);
+const data = fs.readFileSync(`${__dirname}/../firebaseConfig.json`);
+
+process.env[`FIREBASE_CONFIG`] = data
+  .toString()
+  .replaceAll(` `, ``)
+  .replaceAll(`\n`, ``);
+
+console.log(`NEXT_PUBLIC_FIREBASE_CONFIG=${process.env[`FIREBASE_CONFIG`]}`);
 console.log(`Paste this into your .env.local`);
